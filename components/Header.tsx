973aa88d6c035a9fb90f1390e785c38a769b2c43
Carlos Manuel González <cmglezpdev@gmail.com>
2023-06-14 21:59:08 -0400

    feat: add header of codeforces

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export const Header:FC<Props> = () => {
+  return (
+    <div className='w-full'>
+        <div className='w-[95%] bg-international_master m-auto p-2 rounded-md text-sm'>
+            <span className='font-light text-xs'>CODEFORCES</span>
+            <div className='w-full text-center py-7'>
+                <span className='font-bold tracking-[.4rem]'>INTERNATIONAL GRANDMASTER</span>
+            </div>
+            <div className='flex justify-between font-normal'>
+                <span>1188</span>
+                <span>CodeExtreme</span>
+            </div>
+        </div>
+    </div>
+  )
+}
--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,2 @@
 export * from './Contest';
+export * from './Header';
