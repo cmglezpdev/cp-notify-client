127f80f35d6fe6819b77b507bc7cc8b9d254f708
Carlos Manuel González <cmglezpdev@gmail.com>
2023-06-19 00:30:13 -0400

    style: add Codeforces header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,14 +2,14 @@
 export const Header = () => {
   return (
     <div className='w-full'>
-        <div className='w-[95%] bg-international_master m-auto p-2 rounded-md text-sm'>
+        <div className='w-full bg-international_master p-2 rounded-md text-sm'>
             <span className='font-light text-xs'>CODEFORCES</span>
             <div className='w-full text-center py-7'>
                 <span className='font-bold tracking-[.4rem]'>INTERNATIONAL GRANDMASTER</span>
             </div>
             <div className='flex justify-between font-normal'>
                 <span>1188</span>
-                <span>CodeExtreme</span>
+                <span className="font-semibold">CodeExtreme</span>
             </div>
         </div>
     </div>
