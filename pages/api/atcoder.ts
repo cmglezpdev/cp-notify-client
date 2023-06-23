3abf1f4bf27ddbebdb60b14f4cd423502a93a862
Carlos Manuel González <cmglezpdev@gmail.com>
2023-06-23 16:46:43 -0400

    feat: add api to obtain atcoder contests

---
+++ pages/api/atcoder.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { IContest, ICache } from '@/interface';
+import { MemoryCache } from '@/utils';
+import {  } from '../../interface';
+import { scrapAtCoder } from '@/Scrappers';
+
+type Data = 
+| { ok: boolean, message: string }
+| { ok: boolean, contests : IContest[] }
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch(req.method) {
+        case "GET":
+            return getUpcomingContests(req, res);
+        default:
+            res.status(400).json({ ok: false, message: "Bad Request" });
+    }
+}
+
+const cache: ICache = new MemoryCache()
+
+async function getUpcomingContests(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+    const cacheContest: IContest[] | null = cache.get<IContest[]>('atcoder-contests');
+    if(cacheContest) {
+        return res.status(200).json({ ok: true, contests: cacheContest });
+    }
+
+    try {
+        const contests: IContest[] = await scrapAtCoder();
+        cache.set('atcoder-contests', contests);
+        return res.status(200).json({ ok: true, contests });
+    } catch (e) {
+        res.status(400).json({ ok: false, message: 'Fail to obtain the atcoder contests' });
+    }
+}
