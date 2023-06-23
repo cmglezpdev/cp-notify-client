def48fb0edefd3e89e615b0b27ae03065709c101
Carlos Manuel González <cmglezpdev@gmail.com>
2023-06-23 16:46:19 -0400

    feat: add cache to the codeforces contests

--- pages/api/codeforces.ts
+++ pages/api/codeforces.ts
@@ -1,35 +1,52 @@
-import crypto from 'crypto';
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { ICodeforcesContest, ICodeforcesContestsResponse } from '@/interface';
+import { ICodeforcesContestsResponse, IContest } from '@/interface';
+import { MemoryCache } from '@/utils';
+import { ICache } from '../../interface';
 
 type Data = 
-| { message: string }
-| { contests : ICodeforcesContest[] }
+| { ok: boolean, message: string }
+| { ok: boolean, contests : IContest[] }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
     switch(req.method) {
         case "GET":
             return getUpcomingContests(req, res);
         default:
-            res.status(400).json({ message: "Bad Request" });
+            res.status(400).json({ ok: false, message: "Bad Request" });
     }
 }
 
+const cache: ICache = new MemoryCache()
+
 async function getUpcomingContests(req: NextApiRequest, res: NextApiResponse<Data>) {
 
-    const apiKey = '623b17df35b5154d75f1352c24a56a133ea0f26a';
-    const apiSecretKey = 'e3994deb06f46564c5c891d6de6b981ec98dfdf9';
-    const time = Math.round(new Date().getTime() / 1000);
-    const rand = crypto.randomInt(100000, 999999); // 6 digits
-    const toHash = `${rand}/contest.list?apiKey=${apiKey}&time=${time}#${apiSecretKey}`;
-    const hash = crypto.createHash('sha512').update(toHash).digest('hex');
-    const apiSig = `${rand}${hash}`;
+    const cacheContest: IContest[] | null = cache.get<IContest[]>('codeforces-contests');
+    if(cacheContest) {
+        return res.status(200).json({ ok: true, contests: cacheContest });
+    }
+    
+    try {
+        const response:Response = await fetch('https://codeforces.com/api/contest.list', { method: "GET" });
+        const CodeforcesResponse : ICodeforcesContestsResponse = await response.json();
+        const upcomingContests = CodeforcesResponse.result.filter(contest => contest.phase === "BEFORE");
+    
+        upcomingContests.sort((contestA, contestB) => contestA.startTimeSeconds - contestB.startTimeSeconds);
+    
+        const contests : IContest[] = upcomingContests.map(contest => ({
+          id: contest.id,
+          name: contest.name,
+          type: contest.type,
+          platform: 'CODEFORCES',
+          durationSeconds: contest.durationSeconds,
+          startTimeSeconds: contest.startTimeSeconds * 1000,
+          link: `https://codeforces.com/contests/${contest.id}`,
+        }))
+    
+        cache.set('codeforces-contests', contests);
+        return res.status(200).json({ ok: true, contests });
+        
+    } catch (e) {
+        res.status(400).json({ ok: false, message: "Fail to obtain codeforces contests" });
+    }
 
-    const codeforcesUrl = `https://codeforces.com/api/contest.list?apiKey=${apiKey}&time=${time}&apiSig=${apiSig}`;
-
-    const response:Response = await fetch('https://codeforces.com/api/contest.list', { method: "GET" });
-    const CodeforcesResponse : ICodeforcesContestsResponse = await response.json();
-    const upcomingContests = CodeforcesResponse.result.filter(contest => contest.phase === "BEFORE");
-
-    return res.status(200).json({ contests: upcomingContests });
 }
