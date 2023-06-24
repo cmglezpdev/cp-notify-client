165bbd82d0ff9910e06d8956ead7ba4ece42a12b
Carlos Manuel González <cmglezpdev@gmail.com>
2023-06-24 12:13:23 -0400

    feat: add atcoder to getprofile

--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -1,10 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { ICache, IUser, Platform } from '@/interface'
 import { MemoryCache } from '@/utils';
+import { ScrapperAtCoder } from '@/Scrappers';
 
 type Data = 
     | { ok: boolean, message: string }
-    | { ok: boolean, user: IUser }
+    | { ok: boolean, user: IUser | null }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
     switch(req.method) {
@@ -22,12 +23,22 @@ async function getProfile(req: NextApiRequest, res: NextApiResponse<Data>) {
     if(!platform) return res.status(400).json({ ok: false, message: 'The platform is required in the params' });
     if(!handle) return res.status(400).json({ ok: false, message: 'The user handle is required in the params' });
 
-    const url = `https://codeforces.com/api/user.info?handles=${handle}`;
-    const response: Response = await fetch(url, { method: 'GET' });
-    const resp = await response.json();
-    if(resp.status === 'OK') {
-        return res.status(200).json({ ok: true, user: resp.result[0] });
-    } else {
-        return res.status(400).json({ ok: false, message: 'Fail to obtain codeforces user information' });
+    switch( platform as Platform ) {
+        case 'CODEFORCES':
+            const urlCodeforces = `https://codeforces.com/api/user.info?handles=${handle}`;
+            const response: Response = await fetch(urlCodeforces, { method: 'GET' });
+            const resp = await response.json();
+            if(resp.status === 'OK') {
+                return res.status(200).json({ ok: true, user: resp.result[0] });
+            } else {
+                return res.status(400).json({ ok: false, message: 'Fail to obtain codeforces user information' });
+            }
+        case 'ATCODER':
+            const scrapper = new ScrapperAtCoder();
+            const user = await scrapper.getProfile(handle as string);
+            return res.status(200).json({ ok: true, user });
+        default:
+            return res.status(500).json({ ok: false, message: `The platform ${platform} doesn't a valid platform` });
     }
+
 }
