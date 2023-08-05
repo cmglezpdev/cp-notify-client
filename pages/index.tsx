a4152b62b9fde7025ab8333d5f31ef013899c5a9
Carlos Manuel González <cmglezpdev@gmail.com>
2023-08-05 11:45:41 -0400

    feat: modify homepage to use api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,43 @@
-import { AppLayout } from '@/layout';
-import Link from 'next/link'
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
 
-const Home = () => {
+import { AppLayout } from '@/layout';
+import { ApiPlatformResponse, Platform } from '@/types/platform';
+
+interface Props {
+  platforms: Platform[];
+}
+
+function HomePage({ platforms }: Props) {
   return (
-      <AppLayout>
-        <h1 className='font-bold text-3xl text-center'>Online Judges</h1>
-        <div className='flex flex-col gap-2 my-2'>
-          <Link href='/codeforces' className='w-full p-3 border-2 border-black rounded-md text-center cursor-pointer'>
-            CODEFORCES
-          </Link>
-          <Link href='/atcoder' className='w-full p-3 border-2 border-black rounded-md text-center cursor-pointer'>
-            ATCODER
-          </Link>
-        </div>
-      </AppLayout>
+    <AppLayout>
+      <h1 className='font-bold text-3xl text-center'>Online Judges</h1>
+      <div className='flex flex-col gap-2 my-2'>
+        {
+          platforms.map(platform => (
+            <Link
+              key={platform.id}
+              href={`/${platform.name.toLowerCase()}`}
+              className='w-full p-3 border-2 border-black rounded-md text-center cursor-pointer'
+            >
+              {platform.name}
+            </Link>
+          ))
+        }
+      </div>
+    </AppLayout>
   )
 }
 
-export default Home;
+export default HomePage;
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const response = await fetch('http://localhost:4000/api/platform');
+  const { platforms }: ApiPlatformResponse = await response.json();
+
+  return {
+    props: { platforms }
+  }
+}
+
