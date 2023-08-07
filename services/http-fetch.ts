b5561f84c3bc04745f7ad1e8f8dcd864dcbe9460
Carlos Manuel González <cmglezpdev@gmail.com>
2023-08-06 23:48:44 -0400

    feat: add fetch service to abstract the fetchapi

--- services/http-fetch.ts
+++ services/http-fetch.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosError } from "axios";
+
 interface ApiSettings {
     baseUrl?: string;
 }
@@ -9,17 +11,17 @@ abstract class HttpFetchService {
         this.settings = settings;
     }
 
-    async get<T>(link: string): Promise<T> {
+    async get<T>(link: string, settings: any): Promise<T> {
         throw new Error('The method GET not implement yet.')
     }
 
-    async post<T>(link: string, body?: any): Promise<T> {
+    async post<T>(link: string, body?: any, settings?: any): Promise<T> {
         throw new Error('The method POST not implement yet.')
     }
-    async put<T>(link: string, body?: any): Promise<T> {
+    async put<T>(link: string, body?: any, settings?: any): Promise<T> {
         throw new Error('The method PUT not implement yet.')
     }
-    async delete<T>(link: string, body?: any): Promise<T> {
+    async delete<T>(link: string, settings?: any): Promise<T> {
         throw new Error('The method DELET not implement yet.')
     }
 }
@@ -29,34 +31,51 @@ export class HttpServiceImplementation extends HttpFetchService {
         super(settings);
     }
 
-    async get<T>(link: string): Promise<T> {
-        console.log(`${this.settings.baseUrl || ''}${link}`);
-        const response = await fetch(`${this.settings.baseUrl || ''}${link}`);
-        return await response.json();
+    async get<T>(link: string, settings: any = {}): Promise<T> {
+        try {
+            const { data } = await axios.get<T>(`${this.settings.baseUrl || ''}${link}`, {
+                ...settings
+            });
+            return data;
+        } catch (error: any) {
+            throw new Error(error.response!.data.message)
+        }
     }
-    async post<T>(link: string, body?: any): Promise<T> {
-        const response = await fetch(`${this.settings.baseUrl || ''}${link}`, {
-            body,
-            method: 'POST'
-        });
-        return await response.json();
+    async post<T>(link: string, body?: any, settings: any = {}): Promise<T> {
+        try {
+            const { data } = await axios.post<T>(`${this.settings.baseUrl || ''}${link}`, body, {
+                ...settings
+            });
+            return data;
+        } catch (error: any) {
+            throw new Error(error.response!.data.message)
+        }
     }
-    async put<T>(link: string, body?: any): Promise<T> {
-        const response = await fetch(`${this.settings.baseUrl || ''}${link}`, {
-            body,
-            method: 'PUT'
-        });
-        return await response.json();
+
+    async put<T>(link: string, body?: any, settings: any = {}): Promise<T> {
+        try {
+            const { data } = await axios.put<T>(`${this.settings.baseUrl || ''}${link}`, body, {
+                ...settings
+            });
+            return data;
+        } catch (error: any) {
+            throw new Error(error.response!.data.message)
+        }
     }
-    async delete<T>(link: string, body?: any): Promise<T> {
-        const response = await fetch(`${this.settings.baseUrl || ''}${link}`, {
-            body,
-            method: 'DELETE'
-        });
-        return await response.json();
+
+    async delete<T>(link: string, settings: any = {}): Promise<T> {
+        try {
+            const { data } = await axios.delete<T>(`${this.settings.baseUrl || ''}${link}`, {
+                ...settings
+            });
+
+            return data;
+        } catch (error: any) {
+            throw new Error(error.response!.data.message)
+        }
     }
 }
 
 export const httpService = new HttpServiceImplementation({
-    baseUrl: process.env.HOST_API
+    baseUrl: process.env.NEXT_PUBLIC_HOST_API
 })
